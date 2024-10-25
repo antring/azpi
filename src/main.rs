f02124a2ef50b721c50aaa10c22e3ff5bfcf2322
Erik <emauring@gmail.com>
2024-10-25 15:53:23 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use ratatui::crossterm::terminal::enable_raw_mode;
+use std::io::Result;
+
+mod app;
+mod ui;
+
+fn main() -> Result<()> {
+    enable_raw_mode()?;
+
+    
+    let backend = Cross
+    Ok(())
 }
